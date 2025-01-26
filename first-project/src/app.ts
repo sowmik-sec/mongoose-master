1dee5caf930221fa9dced14615e4d2bfaf65e7f4
sowmik-sec <sowmik.sec@gmail.com>
2025-01-26 21:40:01 +0600

    Insert a student data into mongoDB

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 app.use(express.json());
 app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
 
 const getAController = (req: Request, res: Response) => {
   res.send('Hello World!');
--- first-project/src/app/modules/student/student.route.ts
+++ first-project/src/app/modules/student/student.route.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import { StudentControllers } from './student.controller';
 
 const router = express.Router();
 
 // it will call controller func
-router.post('/create-student');
+router.post('/create-student', StudentControllers.createStudent);
+
+export const StudentRoutes = router;
