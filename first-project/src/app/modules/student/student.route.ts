e04fee0bf084d78727ce97fa1c219ac3582246b7
sowmik-sec <sowmik.sec@gmail.com>
2025-01-26 21:53:08 +0600

    Create get student route

--- first-project/src/app/modules/student/student.route.ts
+++ first-project/src/app/modules/student/student.route.ts
@@ -5,5 +5,7 @@ const router = express.Router();
 
 // it will call controller func
 router.post('/create-student', StudentControllers.createStudent);
+router.get('/', StudentControllers.getAllStudents);
+router.get('/:studentId', StudentControllers.getSingleStudent);
 
 export const StudentRoutes = router;
--- first-project/src/app/modules/student/student.service.ts
+++ first-project/src/app/modules/student/student.service.ts
@@ -6,6 +6,18 @@ const createStudentIntoDB = async (student: Student) => {
   return result;
 };
 
+const getAllStudentsFromDb = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
+const getSingleStudentFromDb = async (id: string) => {
+  const result = await StudentModel.findOne({ id });
+  return result;
+};
+
 export const StudentServices = {
   createStudentIntoDB,
+  getAllStudentsFromDb,
+  getSingleStudentFromDb,
 };
