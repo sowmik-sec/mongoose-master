fc04745e01d224befb5da7aa06498b22ce055c6a
sowmik-sec <sowmik.sec@gmail.com>
2025-01-26 21:09:47 +0600

    Create an schema for a student

--- first-project/src/app/modules/student/student.interface.ts
+++ first-project/src/app/modules/student/student.interface.ts
@@ -1,5 +1,3 @@
-import { Schema, model, connect } from 'mongoose';
-
 export type Guardian = {
   fatherName: string;
   fatherOccupation: string;
@@ -8,17 +6,31 @@ export type Guardian = {
   motherOccupation: string;
   motherContactNo: string;
 };
+export type UserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
 export type Student = {
-  name: {
-    firstName: string;
-    lastName: string;
-    middleName: string;
-  };
+  id: string;
+  name: UserName;
   gender: 'male' | 'female';
   email: string;
-  dateOfBirth: string;
+  dateOfBirth?: string;
   contactNo: string;
   emergencyContactNo: string;
   bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
   guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  presentAddress: string;
+  permanentAddress: string;
+  isActive: 'active' | 'block';
 };
---
+++ first-project/src/app/modules/student/student.modal.ts
@@ -0,0 +1,96 @@
+import { Schema, model, connect } from 'mongoose';
+import { Student } from './student.interface';
+
+const studentSchema = new Schema<Student>({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  gender: ['male', 'female'],
+  email: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: String,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: {
+    type: String,
+    required: true,
+  },
+  permanentAddress: {
+    type: String,
+    required: true,
+  },
+  guardian: {
+    fatherName: {
+      type: String,
+      required: true,
+    },
+    fatherOccupation: {
+      type: String,
+      required: true,
+    },
+    fatherContactNo: {
+      type: String,
+      required: true,
+    },
+    motherName: {
+      type: String,
+      required: true,
+    },
+    motherOccupation: {
+      type: String,
+      required: true,
+    },
+    motherContactNo: {
+      type: String,
+      required: true,
+    },
+  },
+  localGuardian: {
+    name: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  emergencyContactNo: {
+    type: String,
+    required: true,
+  },
+  profileImg: {
+    type: String,
+    required: true,
+  },
+  isActive: ['active', 'block'],
+});
