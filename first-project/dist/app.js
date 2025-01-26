1e940040db3f0a24b5a7a30c1aaa667750e8b1cf
sowmik-sec <sowmik.sec@gmail.com>
2025-01-26 20:31:57 +0600

    ..

--- first-project/dist/app.js
+++ first-project/dist/app.js
@@ -1,8 +1,8 @@
-"use strict";
-const express = require("express");
+'use strict';
+const express = require('express');
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 console.log(process.cwd());
