a83fc91ffaf4295a3d9cf9db750dbed13657bdcb
sowmik-sec <sowmik.sec@gmail.com>
2025-01-26 20:53:30 +0600

    create student interface

---
+++ first-project/src/app/modules/student/student.interface.ts
@@ -0,0 +1,24 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+export type Student = {
+  name: {
+    firstName: string;
+    lastName: string;
+    middleName: string;
+  };
+  gender: 'male' | 'female';
+  email: string;
+  dateOfBirth: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  guardian: Guardian;
+};
