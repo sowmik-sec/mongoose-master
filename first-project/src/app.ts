b47e28d94345c9c54b24286abe4d32e24c38962e
sowmik-sec <sowmik.sec@gmail.com>
2025-01-31 16:40:37 +0600

    Create index route and module summary

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -1,17 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 const getAController = (req: Request, res: Response) => {
   res.send('Hello World!');
