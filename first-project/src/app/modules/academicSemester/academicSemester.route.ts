c3f1ae3d945d2c1449f2cfa62691fbc63651bb29
sowmik-sec <sowmik.sec@gmail.com>
2025-02-01 15:08:37 +0600

    Create Academic Semester service

--- first-project/src/app/modules/academicSemester/academicSemester.route.ts
+++ first-project/src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { AcademicSemesterControllers } from './academicSemester.controller';
+import validateRequest from '../../utils/validateRequest';
+import { AcademicSemesterValidations } from './academicSemester.validation';
 
 const router = express.Router();
 
 // it will call controller func
 router.post(
   '/create-academic-semester',
+  validateRequest(
+    AcademicSemesterValidations.createAcademicSemesterValidationSchema,
+  ),
   AcademicSemesterControllers.createAcademicSemester,
 );
 export const AcademicSemesterRoutes = router;
--- first-project/src/app/modules/academicSemester/academicSemester.validation.ts
+++ first-project/src/app/modules/academicSemester/academicSemester.validation.ts
@@ -8,7 +8,7 @@ import {
 const createAcademicSemesterValidationSchema = z.object({
   body: z.object({
     name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
-    year: z.date(),
+    year: z.string(),
     code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
     startMonth: z.enum([...Months] as [string, ...string[]]),
     endMonth: z.enum([...Months] as [string, ...string[]]),
@@ -18,7 +18,7 @@ const createAcademicSemesterValidationSchema = z.object({
 const updateAcademicSemesterValidationSchema = z.object({
   body: z.object({
     name: z.enum([...AcademicSemesterName] as [string, ...string[]]).optional(),
-    year: z.date().optional(),
+    year: z.string().optional(),
     code: z.enum([...AcademicSemesterCode] as [string, ...string[]]).optional(),
     startMonth: z.enum([...Months] as [string, ...string[]]).optional(),
     endMonth: z.enum([...Months] as [string, ...string[]]).optional(),
