09e7a459750bbed5a7f4aac071c10939a4598973
sowmik-sec <sowmik.sec@gmail.com>
2025-02-01 15:24:13 +0600

    Handle academic semester logical validation

--- first-project/src/app/modules/academicSemester/academicSemester.service.ts
+++ first-project/src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,14 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDb = async (payload: TAcademicSemester) => {
+  // semester name --> semester code
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid semester code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
