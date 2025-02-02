8c81589396b712d863593be64b21d2363cfff957
sowmik-sec <sowmik.sec@gmail.com>
2025-02-02 12:42:15 +0600

    Handle department validation when creating _ updating document

--- first-project/src/app/modules/academicDepartment/academicDepartment.model.ts
+++ first-project/src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,25 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error('This department is already exists');
+  }
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExists) {
+    throw new Error('This department does not exists');
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
