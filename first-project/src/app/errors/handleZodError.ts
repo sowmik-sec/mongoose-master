f382f81e1f09fb75d0d44163747591c894642af6
sowmik-sec <sowmik.sec@gmail.com>
2025-02-03 22:27:03 +0600

    handle castError

--- first-project/src/app/errors/handleZodError.ts
+++ first-project/src/app/errors/handleZodError.ts
@@ -1,6 +1,7 @@
 import { ZodError, ZodIssue } from 'zod';
+import { TGenericErrorResponse } from '../interfaces/error';
 
-const handleZodError = (err: ZodError) => {
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
   const errorSources = err?.issues.map((issue: ZodIssue) => {
     return {
       path: issue?.path[issue.path.length - 1],
--- first-project/src/app/middlewares/globalErrorHandler.ts
+++ first-project/src/app/middlewares/globalErrorHandler.ts
@@ -5,6 +5,7 @@ import { TErrorSources } from '../interfaces/error';
 import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
@@ -26,6 +27,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
