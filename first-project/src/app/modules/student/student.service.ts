7799b1ae232c56ee28331bab842a5b72ff1d41fb
sowmik-sec <sowmik.sec@gmail.com>
2025-02-03 23:36:08 +0600

    How to do raw filtering ,sorting and limiting

--- first-project/src/app/modules/student/student.service.ts
+++ first-project/src/app/modules/student/student.service.ts
@@ -7,16 +7,26 @@ import { TStudent } from './student.interface';
 
 const getAllStudentsFromDb = async (query: Record<string, unknown>) => {
   // {email: {$regex: query.searchTerm, $options: i}}
+  console.log('base query', query);
+  const queryObj = { ...query }; // copy
+  const studentSearchableFields = ['email', 'name.firstName', 'presentAddress'];
   let searchTerm = '';
   if (query?.searchTerm) {
     searchTerm = query?.searchTerm as string;
   }
 
-  const result = await Student.find({
-    $or: ['email', 'name.firstName', 'presentAddress'].map((field) => ({
+  const searchQuery = Student.find({
+    $or: studentSearchableFields.map((field) => ({
       [field]: { $regex: searchTerm, $options: 'i' },
     })),
-  })
+  });
+
+  // filtering
+  const excludeFields = ['searchTerm', 'sort', 'limit'];
+  excludeFields.forEach((el) => delete queryObj[el]);
+
+  const filterQuery = searchQuery
+    .find(queryObj)
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
@@ -24,7 +34,19 @@ const getAllStudentsFromDb = async (query: Record<string, unknown>) => {
         path: 'academicFaculty',
       },
     });
-  return result;
+  let sort = '-createdAt';
+  if (query?.sort) {
+    sort = query?.sort as string;
+  }
+  const sortQuery = filterQuery.sort(sort);
+
+  let limit = 1;
+  if (query?.limit) {
+    limit = query.limit as number;
+  }
+  const limitQuery = await sortQuery.limit(limit);
+
+  return limitQuery;
 };
 
 const getSingleStudentFromDb = async (id: string) => {
