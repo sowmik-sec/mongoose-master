02e002350b6f02c5c976ad9ff834fabd75ede38b
sowmik-sec <sowmik.sec@gmail.com>
2025-02-04 19:16:46 +0600

    Use transaction for updating course

--- first-project/src/app/modules/course/course.model.ts
+++ first-project/src/app/modules/course/course.model.ts
@@ -1,39 +1,49 @@
 import { model, Schema } from 'mongoose';
 import { TCourse, TPreRequisiteCourses } from './course.interface';
 
-const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: 'Course',
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>(
+  {
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isDeleted: {
-    type: Boolean,
-    default: false,
+  {
+    _id: false,
   },
-});
+);
 
-const courseSchema = new Schema<TCourse>({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const courseSchema = new Schema<TCourse>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    prefix: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    credits: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  prefix: {
-    type: String,
-    required: true,
-    trim: true,
+  {
+    timestamps: true,
   },
-  credits: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  preRequisiteCourses: [preRequisiteCoursesSchema],
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
 
 export const Course = model<TCourse>('Course', courseSchema);
