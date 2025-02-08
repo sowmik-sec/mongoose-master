08af0db3f1b83c94b28cdc37ea7a77b8b8c213cb
sowmik-sec <sowmik.sec@gmail.com>
2025-02-08 12:21:25 +0600

    use routes in app

--- assignment-3/src/app.ts
+++ assignment-3/src/app.ts
@@ -3,11 +3,13 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routes";
 
 app.use(express.json());
 app.use(cors());
 
 // application routes
+app.use("/api/v1", router);
 
 const getAController = (req: Request, res: Response) => {
   res.send("Hello World!");
