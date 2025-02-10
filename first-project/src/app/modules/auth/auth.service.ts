a8f8ad282d4e701bc8f5a943e7bc7dd4fbfb75f1
sowmik-sec <sowmik.sec@gmail.com>
2025-02-10 17:21:57 +0600

    Intro to JWT , create an accessToken

--- first-project/src/app/modules/auth/auth.service.ts
+++ first-project/src/app/modules/auth/auth.service.ts
@@ -2,33 +2,43 @@ import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 const loginUser = async (payload: TLoginUser) => {
   // check if the user exists
-  const isUserExists = await User.isUserExistsByCustomId(payload.id);
-  if (!isUserExists) {
+  const user = await User.isUserExistsByCustomId(payload.id);
+  if (!user) {
     throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
   }
   // check if the user is already deleted
-  // const isDeleted = isUserExists?.isDeleted;
-  // if (isDeleted) {
-  //   throw new AppError(StatusCodes.FORBIDDEN, 'User is deleted');
-  // }
+  const isDeleted = user?.isDeleted;
+  if (isDeleted) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'User is deleted');
+  }
   // // check if the user is blocked
-  // const userStatus = isUserExists?.status;
-  // if (userStatus === 'blocked') {
-  //   throw new AppError(StatusCodes.FORBIDDEN, 'User is blocked');
-  // }
+  const userStatus = user?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(StatusCodes.FORBIDDEN, 'User is blocked');
+  }
 
   // checking if the password is correct
   const isPasswordMatched = await User.isPasswordMatched(
     payload.password,
-    isUserExists.password,
+    user.password,
   );
   if (!isPasswordMatched) {
     throw new AppError(StatusCodes.FORBIDDEN, 'Password do not matched');
   }
   // Access Granted: Send accessToken, RefreshToken
-  return {};
+  // create token and sent to the client
+  const jwtPayload = {
+    userId: user.id,
+    role: user.role,
+  };
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+  return { accessToken, needsPasswordChange: user.needsPasswordChange };
 };
 
 export const AuthServices = {
