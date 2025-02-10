09275fbbf8f1b413423237fa2b08bd748a610dec
sowmik-sec <sowmik.sec@gmail.com>
2025-02-10 17:07:00 +0600

    Implement statics for validation

--- first-project/src/app/modules/auth/auth.service.ts
+++ first-project/src/app/modules/auth/auth.service.ts
@@ -2,30 +2,31 @@ import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
-import bcrypt from 'bcrypt';
 const loginUser = async (payload: TLoginUser) => {
   // check if the user exists
-  const isUserExists = await User.findOne({ id: payload.id });
+  const isUserExists = await User.isUserExistsByCustomId(payload.id);
   if (!isUserExists) {
     throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
   }
   // check if the user is already deleted
-  const isDeleted = isUserExists?.isDeleted;
-  if (isDeleted) {
-    throw new AppError(StatusCodes.FORBIDDEN, 'User is deleted');
-  }
-  // check if the user is blocked
-  const userStatus = isUserExists?.status;
-  if (userStatus === 'blocked') {
-    throw new AppError(StatusCodes.FORBIDDEN, 'User is blocked');
-  }
+  // const isDeleted = isUserExists?.isDeleted;
+  // if (isDeleted) {
+  //   throw new AppError(StatusCodes.FORBIDDEN, 'User is deleted');
+  // }
+  // // check if the user is blocked
+  // const userStatus = isUserExists?.status;
+  // if (userStatus === 'blocked') {
+  //   throw new AppError(StatusCodes.FORBIDDEN, 'User is blocked');
+  // }
 
   // checking if the password is correct
-  const isPasswordMatched = await bcrypt.compare(
+  const isPasswordMatched = await User.isPasswordMatched(
     payload.password,
-    isUserExists?.password,
+    isUserExists.password,
   );
-
+  if (!isPasswordMatched) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'Password do not matched');
+  }
   // Access Granted: Send accessToken, RefreshToken
   return {};
 };
--- first-project/src/app/modules/user/user.model.ts
+++ first-project/src/app/modules/user/user.model.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from 'mongoose';
-import { TUser } from './user.interface';
+import { TUser, UserModel } from './user.interface';
 import bcrypt from 'bcrypt';
 import config from '../../config';
 
-const userSchema = new Schema<TUser>(
+const userSchema = new Schema<TUser, UserModel>(
   {
     id: {
       type: String,
@@ -54,4 +54,15 @@ userSchema.post('save', function (doc, next) {
   next();
 });
 
-export const User = model<TUser>('User', userSchema);
+userSchema.statics.isUserExistsByCustomId = async function (id: string) {
+  return await User.findOne({ id });
+};
+
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword,
+  hashedPassword,
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+export const User = model<TUser, UserModel>('User', userSchema);
