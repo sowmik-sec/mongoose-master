8974e56d676b60f6d613f7e6eac0286be7870a40
sowmik-sec <sowmik.sec@gmail.com>
2025-02-10 17:35:44 +0600

    Create auth middleware

---
+++ first-project/src/app/middlewares/auth.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import { StatusCodes } from 'http-status-codes';
+
+const auth = () => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
+    }
+    next();
+  });
+};
+
+export default auth;
--- first-project/src/app/modules/student/student.route.ts
+++ first-project/src/app/modules/student/student.route.ts
@@ -2,17 +2,17 @@ import express from 'express';
 import { StudentControllers } from './student.controller';
 import validateRequest from '../../utils/validateRequest';
 import { studentValidations } from './student.validation';
-
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
 // it will call controller func
-router.get('/', StudentControllers.getAllStudents);
 router.get('/:id', StudentControllers.getSingleStudent);
 router.patch(
   '/:id',
   validateRequest(studentValidations.updateStudentValidationSchema),
   StudentControllers.updateStudent,
 );
+router.get('/', auth(), StudentControllers.getAllStudents);
 router.delete('/:id', StudentControllers.deleteSingleStudent);
 
 export const StudentRoutes = router;
