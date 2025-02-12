188fed54fda43318f0df6ee477cd6a0d4b45ab43
sowmik-sec <sowmik.sec@gmail.com>
2025-02-12 11:47:30 +0600

    Add a student into enrolled course

--- first-project/src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ first-project/src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/AppError';
 import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import EnrolledCourse from './enrolledCourse.model';
 import { Student } from '../student/student.modal';
+import mongoose from 'mongoose';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
@@ -34,6 +36,45 @@ const createEnrolledCourseIntoDB = async (
   });
   if (isStudentAlreadyEnrolled) {
     throw new AppError(StatusCodes.CONFLICT, 'Student already enrolled');
+  }
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    const result = await EnrolledCourse.create(
+      [
+        {
+          semesterRegistration: isOfferedCourseExists.semesterRegistration,
+          academicSemester: isOfferedCourseExists.academicSemester,
+          academicFaculty: isOfferedCourseExists.academicFaculty,
+          academicDepartment: isOfferedCourseExists.academicDepartment,
+          offeredCourse: offeredCourse,
+          course: isOfferedCourseExists.course,
+          student: student._id,
+          faculty: isOfferedCourseExists.faculty,
+          isEnrolled: true,
+        },
+      ],
+      { session },
+    );
+    if (!result) {
+      throw new AppError(
+        StatusCodes.BAD_REQUEST,
+        'Failed to enroll in this course',
+      );
+    }
+    const maxCapacity = isOfferedCourseExists.maxCapacity;
+    await OfferedCourse.findByIdAndUpdate(
+      offeredCourse,
+      { maxCapacity: maxCapacity - 1 },
+      { new: true, session },
+    );
+    await session.commitTransaction();
+    await session.endSession();
+    return result;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(error);
   }
 };
 
