832eea96cc2c9eacbd4a11ad10b630cfaa0ce56d
sowmik-sec <sowmik.sec@gmail.com>
2025-02-15 17:33:51 +0600

    create auth interface and validation

---
+++ assignment-4/src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    username: z.string({ required_error: "Username is required" }),
+    password: z.string({ required_error: "Password is required" }),
+  }),
+});
+
+export const UserValidations = {
+  createUserZodSchema,
+};
