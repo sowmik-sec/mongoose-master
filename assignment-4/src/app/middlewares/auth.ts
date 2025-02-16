b0318c0df618ab75fbc75b57b00a7c9a4d3a7402
sowmik-sec <sowmik.sec@gmail.com>
2025-02-16 20:18:26 +0600

    create auth middleware

--- assignment-4/src/app/middlewares/auth.ts
+++ assignment-4/src/app/middlewares/auth.ts
@@ -1,3 +1,44 @@
-const auth = (...roles: string[]) => {};
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../utils/catchAsync";
+import AppError from "../errors/AppError";
+import { StatusCodes } from "http-status-codes";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { User } from "../modules/user/user.model";
+
+const auth = (...roles: string[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, "You are not authorized");
+    }
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string
+    ) as JwtPayload;
+    const { role, _id, iat } = decoded;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+    }
+    if (
+      user.passwordChangedAt &&
+      User.isJWTIssuedBeforePasswordChanged(
+        user.passwordChangedAt,
+        iat as number
+      )
+    ) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, "You are not authorized");
+    }
+
+    if (roles && !roles.includes(role)) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, "You are not authorized");
+    }
+    req.user = decoded as JwtPayload & { role: string };
+
+    next();
+  });
+};
 
 export default auth;
